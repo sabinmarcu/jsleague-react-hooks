5c0e14642052411254575b59c3bac5fd633ff8ed
Sabin Marcu <sabinmarcu@gmail.com>
2019-11-23 08:17:45 +0200

    Initial slides push

---
+++ docs/react-syntax-highlighter_languages_highlight_plaintext.render-page.js
@@ -0,0 +1,20 @@
+exports.ids = ["react-syntax-highlighter_languages_highlight_plaintext"];
+exports.modules = {
+
+/***/ "../../node_modules/react-syntax-highlighter/node_modules/highlight.js/lib/languages/plaintext.js":
+/*!**********************************************************************************************************************************************!*\
+  !*** C:/Users/sabin/Desktop/jsleague-react-hooks/node_modules/react-syntax-highlighter/node_modules/highlight.js/lib/languages/plaintext.js ***!
+  \**********************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = function(hljs) {
+    return {
+        disableAutodetect: true
+    };
+};
+
+/***/ })
+
+};;
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.render-page.js.map
